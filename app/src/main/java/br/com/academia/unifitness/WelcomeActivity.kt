8c550e168662cbdce1b57a5b06f088261955184c
danielluccio <daniel130877@gmail.com>
2025-04-22 14:53:44 -0300

    Tela de Home

---
+++ app/src/main/java/br/com/academia/unifitness/WelcomeActivity.kt
@@ -0,0 +1,15 @@
+package br.com.academia.unifitness
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.academia.unifitness.R
+
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+
+
+}
