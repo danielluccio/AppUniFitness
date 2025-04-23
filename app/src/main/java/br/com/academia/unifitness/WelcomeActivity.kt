6422b68d63f8e6bf0d70fbcd33f4ae9888503749
danielluccio <daniel130877@gmail.com>
2025-04-23 16:56:33 -0300

    Ajustes la tela de login

--- app/src/main/java/br/com/academia/unifitness/WelcomeActivity.kt
+++ app/src/main/java/br/com/academia/unifitness/WelcomeActivity.kt
@@ -1,15 +1,20 @@
 package br.com.academia.unifitness
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import br.com.academia.unifitness.R
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class WelcomeActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar)
+
+        btnEntrar.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-
 }
